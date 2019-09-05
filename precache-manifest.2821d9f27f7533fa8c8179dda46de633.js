5d7b851cc9bffcfd6cfd036281373358ca007eb7
allytang <allytang2015@gmail.com>
2019-09-05 23:33:14 +1000

    Updates

---
+++ precache-manifest.2821d9f27f7533fa8c8179dda46de633.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "41e41696e17b410514ae936d2d6199ee",
+    "url": "/react-weather-app/index.html"
+  },
+  {
+    "revision": "a6ba4aa149459ee1796e",
+    "url": "/react-weather-app/static/css/2.266e55a5.chunk.css"
+  },
+  {
+    "revision": "a6ba4aa149459ee1796e",
+    "url": "/react-weather-app/static/js/2.f5b53575.chunk.js"
+  },
+  {
+    "revision": "8d8ed77976576db9c3e0",
+    "url": "/react-weather-app/static/js/main.fdca10a5.chunk.js"
+  },
+  {
+    "revision": "654aea34005939a42790",
+    "url": "/react-weather-app/static/js/runtime~main.6b47b80c.js"
+  },
+  {
+    "revision": "b6796c732547b6b7e19cdc8e4ad58356",
+    "url": "/react-weather-app/static/media/bg.b6796c73.jpg"
+  },
+  {
+    "revision": "305eba92086731db4fa483eac13e5dc1",
+    "url": "/react-weather-app/static/media/headerPig.305eba92.jpg"
+  }
+]);
